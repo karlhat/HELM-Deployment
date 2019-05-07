af5dd5c3b585b24c95d7c85029d0e706f9ec4e30
karlhat <36545488+karlhat@users.noreply.github.com>
2019-05-07 16:32:28 -0500

    Create install-helm.sh

---
+++ install-helm.sh
@@ -0,0 +1,14 @@
+echo "Installing oraclelinux-developer-release-el7 package" 
+echo ""
+yum install -y oraclelinux-developer-release-el7
+if [ $? -eq 0 ]
+then
+   echo "running /usr/bin/ol_yum_configure.sh"
+   /usr/bin/ol_yum_configure.sh
+   echo "enabling  ol7_developer repo"
+   yum -y install yum-utils
+   yum-config-manager --enable  ol7_developer
+   echo "installing HELM"
+   yum install helm
+   
+fi
