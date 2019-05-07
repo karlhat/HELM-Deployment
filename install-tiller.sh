474a931deb11de3b5de4aeaecd94dbbecee745d1
karlhat <36545488+karlhat@users.noreply.github.com>
2019-05-07 16:44:04 -0500

    Update install-tiller.sh

--- install-tiller.sh
+++ install-tiller.sh
@@ -1,8 +1,11 @@
 !#/bin/bash
 echo "creating serviceaccount tiller"
 kubectl create serviceaccount --namespace kube-system tiller
+echo "Binding the serviceaccount to the admin role called cluster-admin inside the kube-system namespace"
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 echo "Initiating HELM"
 helm init --tiller-image=container-registry.oracle.com/kubernetes_developer/tiller:v2.9.1 --service-account tiller
 echo "updating HELM repo"
 helm repo update
+echo "Updating the tiller-deploy deployment to have the service account"
+kubectl -n kube-system patch deployment tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
