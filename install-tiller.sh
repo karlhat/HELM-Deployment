b07e9e0104aae84d786661d5dae2bfe526dde606
karlhat <36545488+karlhat@users.noreply.github.com>
2019-05-08 11:25:30 -0500

    Update install-tiller.sh

--- install-tiller.sh
+++ install-tiller.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+if [ $UID -ne 0 ]
+then
 echo "creating serviceaccount tiller"
 kubectl create serviceaccount --namespace kube-system tiller
 echo "Binding the serviceaccount to the admin role called cluster-admin inside the kube-system namespace"
@@ -9,3 +11,5 @@ echo "updating HELM repo"
 helm repo update
 echo "Updating the tiller-deploy deployment to have the service account"
 kubectl -n kube-system patch deployment tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+fi
+echo "Run this script as non-root user"
